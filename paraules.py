f25215e2d363dfecd74e45256bac4c4cc31aef91
Arkania221 <angeloki1963@gmail.com>
2024-01-11 08:46:27 +0100

    Commit de la correccion "paraules" , tengo problemas con el git xd

---
+++ paraules.py
@@ -0,0 +1,13 @@
+for caracter in SEPARADORES:
+        texto = texto.replace(caracter, '')
+
+palabras = texto.split()
+
+palabras_sin_repetir =  set(palabras)
+
+total_palabras = 0
+
+for palabras in palabras_sin_repetir:
+        total_palabras += palabras.count(palabra)
+
+print(total_palabras, len(palabras))
