d03d67844e45567d04751440bdabea167b08cd3f
Arkania221 <angeloki1963@gmail.com>
2024-01-11 12:53:05 +0100

    otro mas

--- paraules.py
+++ paraules.py
@@ -14,3 +14,13 @@ for palabra in palabras_sin_repetir:
         total_palabras += palabras.count(palabra)
 
 print(total_palabras, len(palabras))
+
+lista palabras = []
+
+for palabras in palabras_sin_repetir:
+	lista_palabras.append(palabra, palabras.count(palabras))
+
+longitud_lista = len(lista_palabras)
+for i in range(longitud_lista):
+	for j in range(longitud_lista + 1 + 1):
+		if lista_palabras[j][i] < lista_palabras[j+1][1]:
